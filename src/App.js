19cea35f1eb102b940ed225101215eabe6e01c4d
canasmh <canasmh@yahoo.com>
2022-10-11 22:17:01 -0400

    Loop through data and render a component for each character

--- src/App.js
+++ src/App.js
@@ -1,6 +1,22 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import Character from './components/Character';
+import axios from 'axios';
 
 const App = () => {
+
+  const [characters, setCharacters] = useState([])
+
+  useEffect(() => {
+    axios
+      .get("https://swapi.dev/api/people/")
+      .then((res) => {
+        setCharacters(res.data);
+        console.log(res.data);
+      })
+      .catch(err => console.log(`There was an error: ${err}`));
+
+    return () => console.log("Cleaned side effect");
+  }, []);
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
 
@@ -11,6 +27,8 @@ const App = () => {
   return (
     <div className="App">
       <h1 className="Header">Characters</h1>
+      {characters.map((character, index) => <Character data={character} key={index} />)}
+      
     </div>
   );
 }
